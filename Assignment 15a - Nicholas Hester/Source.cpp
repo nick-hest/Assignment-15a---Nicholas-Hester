4d151aab60a6f499b41a5d7f709ca3a11c175a3c
NickHest <nick@hest.com>
2022-12-03 15:12:34 -0500

    Worked on character function's code.

--- Assignment 15a - Nicholas Hester/Source.cpp
+++ Assignment 15a - Nicholas Hester/Source.cpp
@@ -10,3 +10,35 @@ int main()
 {
 	return 0;
 }
+
+char character(char start, int offset)
+{
+	try
+	{
+		if (!((start >= 'a' && start <= 'z') || (start >= 'A' && start <= 'Z')))
+			throw invalidRangeException();
+	}
+	catch (invalidRangeException)
+	{
+		cout << "Invalid Range Exception";
+		return ' ';
+	}
+
+	bool place;
+	if (start >= 'a' && start <= 'z')
+		place = true;
+	else
+		place = false;
+
+	try
+	{
+		if (place && start + offset < 'a' || place && start + offset>'z' || !place && start + offset < 'A' || !place && start + offset>'Z')
+			throw invalidRangeException();
+	}
+	catch (invalidRangeException)
+	{
+		cout << "Invalid Range Exception";
+		return ' ';
+	}
+	return start + offset;
+}
